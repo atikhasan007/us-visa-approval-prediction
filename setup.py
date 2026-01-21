7e011a6f88fc4d0b8a790c28395cfc66e2e0d7a7
Md Atik Hasan <atikhasan007@gmail.com>
2026-01-21 17:15:54 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+
+    name="us-visa",
+    version="0.0.0",
+    author="atik",
+    author_email="imatik513@gmail.com",
+    packages=find_packages()
+    
+)
